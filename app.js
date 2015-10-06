0306da096bf5dff49b8719e2537862f1e468793b
Noland Reed <Chase.reed@mac.com>
2015-10-06 16:23:18 -0700

    messed around with style-guide and css

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 
 var cookieStand = function(place, min, max, avgCookieCust) {
-      //var pikePlace = {
   this.location = location;
-        //location: 'Pike Place Market',
   this.min = min;
-        //min: 17,
   this.max = max;
-        //max: 88,
   this.avgCookieCust = avgCookieCust;
-        //avgCookieCust: 5.2,
   this.totalCookieHour = [];
-        //totalCookieHour: [],
   this.totalCookieDay = [];
-        //totalCookieDay: [],
   this.hours = ['10am', '11am', '12pm','1pm','2pm','3pm','4pm','5pm'];
-        //hours: ['10am', '11am', '12pm','1pm','2pm','3pm','4pm','5pm'],
   this.randCustHour = function() {
     return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-        // randCustHour: function() {
-        //   return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min
   };
   this.makeUL = function(id) {
     var total = 0;
@@ -27,10 +17,6 @@ var cookieStand = function(place, min, max, avgCookieCust) {
     for( i = 0; i < 8; i++) {
       this.totalCookieHour[i] = Math.floor(this.randCustHour() * this.avgCookieCust);
       console.log(id)
-        // makeUL: function(id) {
-        //   var total = 0;
-        //   for( i = 0; i < 8; i++) {
-        //     this.totalCookieHour[i] = Math.floor(this.randCustHour() * this.avgCookieCust))
       var item = document.createElement('li');
       item.appendChild(document.createTextNode(this.hours[i] + ': ' + this.totalCookieHour[i] + " cookies"));
       parent.appendChild(item);
@@ -42,7 +28,6 @@ var cookieStand = function(place, min, max, avgCookieCust) {
     parent.appendChild(itemTotal);
   };
 };
-
 var pikePlace = new cookieStand('Pike Place Market', 17, 88, 5.2);
 pikePlace.makeUL('pike');
 var seatac = new cookieStand('SeaTac Airport', 6, 44, 1.2);
@@ -53,6 +38,3 @@ var bellevue = new cookieStand('Bellevue square', 20, 48, 3.3);
 bellevue.makeUL('bellevue')
 var alki = new cookieStand('Alki', 3, 24, 2.6);
 alki.makeUL('alki')
-// git checkout -b monday
-// git checkout -b tuesday-contrutor-table
-// git push origin tuesday-contrutor-table
